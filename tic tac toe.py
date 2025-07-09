1ebcb244775c1a1ddf271645e9b05f0780ccd8f6
Susan Mirja <susanmirja56@gmail.com>
2025-07-09 18:39:31 +0530

    Update tic tac toe.py

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,1 +1,15 @@
+import tkinter as tk
+from tkinter import messagebox
+import copy
 
+class TicTacToe(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Tic Tac Toe - Ritabrata")
+        self.geometry("300x300")
+        self.board = [[' ' for _ in range(3)] for _ in range(3)]
+        self.buttons = [[None for _ in range(3)] for _ in range(3)]
+        self.current_player = 'X'
+        self.create_board()
+
+   
